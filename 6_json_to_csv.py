0af87b0a84cad54949acf8c258779e2252b37a02
Andrey <8022448+pLINaROF@users.noreply.github.com>
2021-08-15 22:30:54 +0300

    Update 6_json_to_csv.py

--- 6_json_to_csv.py
+++ 6_json_to_csv.py
@@ -2,3 +2,6 @@ import pandas
 
 df = pandas.read_json('data_with_income_rub.json')
 df.to_csv('data_with_income_rub.csv', index=False)
+
+df = pandas.read_json('data_with_income_rub_from_csv.json')
+df.to_csv('data_with_income_rub_from_csv.csv', index=False)
