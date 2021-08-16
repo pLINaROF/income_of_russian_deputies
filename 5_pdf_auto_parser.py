1b3fc85a40058fd8329d0f65b72a9ea1cc62d28e
Andrey <8022448+pLINaROF@users.noreply.github.com>
2021-08-16 12:43:52 +0300

    Update 5_pdf_auto_parser.py

--- 5_pdf_auto_parser.py
+++ 5_pdf_auto_parser.py
@@ -7,33 +7,24 @@ import glob
 
 pdf_files = glob.glob(r'pdf/*.pdf')
 n = 1
-for pdf_file in pdf_files:
-    csv_file = str(pdf_file).replace('.pdf', '.csv')
-    print(f'#{n}', f'"{pdf_file}"', '\tto\t', f'"{csv_file}"', 'done')
-    tabula.convert_into(pdf_file, csv_file, output_format="csv", pages='all')
-    n += 1
-print('Смена формата завершена')
-
-
-print('Получаем доход из csv')
 data_dict = []
 problem_dict = []
-csv_files = glob.glob(r'pdf/*.csv')
-for csv_file in csv_files:
-    deputy_id, deputy_name, consignment_name, income_year = csv_file.split('; ')
-    deputy_id = int(str(deputy_id).split('\\')[1])
-    df = pandas.read_csv(csv_file, encoding='windows-1251', index_col=False)
 
+for pdf_file in pdf_files:
+    pdf_df = tabula.read_pdf(pdf_file, pages='all', lattice=False)
+    deputy_id, deputy_name, consignment_name, income_year = pdf_file.split('; ')
+    deputy_id = int(str(deputy_id).split('\\')[1])
     try:
-        income_sum = df.iloc[0].values[1]
+        income_sum = pdf_df[0].iloc[0].values[1]
         income_sum = int(re.sub(r'(\..+|,.+)', '', income_sum))
-        # print(csv_file, income_sum, 'руб')
+        # print(f'#{n}', pdf_file, income_sum, 'руб')
     except TypeError:
         income_sum = 'требуется проверить вручную'
-        print('Не получилось достать доход из файла')
-        print(csv_file)
+        print(f'#{n}, {pdf_file} \tНе получилось достать доход из файла. '
+              f'Требуется проверить вручную и добавить в json')
         problem_dict.append([deputy_id, deputy_name, f'http://duma.gov.ru/duma/persons/{deputy_id}/property/'])
-
+    finally:
+        print(f'#{n}', pdf_file, income_sum)
     data = {
         'id': deputy_id,
         'deputy_name': deputy_name,
@@ -42,13 +33,12 @@ for csv_file in csv_files:
         'income_sum': income_sum
     }
     data_dict.append(data)
-
-    with open('data_with_income_rub_from_csv.json', 'w', encoding='utf-8') as json_file:
+    with open('data_with_income_rub.json', 'w', encoding='utf-8') as json_file:
         json.dump(data_dict, json_file, indent=4, ensure_ascii=False)
+    n += 1
 
 print('\n', '=' * 100)
 print('Не удалось получить доход по депутатам, нужно проверить руками и добавить в json\n'
       'в json проблемные записи искать по "требуется проверить вручную"')
 for p in problem_dict:
     print(p)
-
